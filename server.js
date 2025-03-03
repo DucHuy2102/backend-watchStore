6358df57a89180e5ccbd3896d8c725f94e99da33
Duc Huy Nguyen <duchuytv2102@gmail.com>
2025-03-03 12:34:49 +0700

    connect to database successfully

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDb } from './database/connectDb.js';
 
 const app = express();
 dotenv.config();
@@ -7,5 +8,5 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    // connectDb();
+    connectDb();
 });
